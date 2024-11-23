f1f9c2e38057868edd3c7795eba169c1e5a3f4d9
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2024-11-23 10:31:09 -0500

    set up python file

--- expense-sheet-tracker.py
+++ expense-sheet-tracker.py
@@ -1,2 +1,10 @@
+import argparse
+
 if __name__ == "__main__":
-    print('hi')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('creditTD')
+    parser.add_argument('creditAE')
+    parser.add_argument('debitTD')
+    args = parser.parse_args()
+
+    print(args.creditTD, args.creditAE, args.debitTD)
