27fcbd6c947cdf8e10a9545f65fdad4164989c6a
Ctrl-Sky <scquan23@gmail.com>
2024-12-23 14:22:45 -0500

    only keep etransfer info

--- expense-sheet-tracker.py
+++ expense-sheet-tracker.py
@@ -13,45 +13,52 @@ import csv
 #     convert date time back to string 
 #     write to excel file
 
-def columnToDateTime(df, columnName, format):
+def column_to_date_time(df, columnName, format):
     df[columnName] = pd.to_datetime(df[columnName], format=format)
 
-def insertNewColumn(df, position, name, values):
+def insert_new_column(df, position, name, values):
     df.insert(position, name, values)
 
-def removeCreditCardThankYou(df):
+def remove_credit_card_thank_you(df):
     filter = df["Description"].str.contains("THANK YOU")
-    print(type(filter))
     return df[~filter]
 
-def setUpCreditAE(credit_ae_sheet):
+def setup_credit_AE(credit_ae_sheet):
     df = pd.read_excel(credit_ae_sheet, header=11)
-    df = removeCreditCardThankYou(df)
+    df = remove_credit_card_thank_you(df)
     df['Description'] = df['Description'].str.lstrip('=')
     df['Amount'] = df['Amount'].str.replace('$', '')
     df['Amount'] = df['Amount'].str.replace(',', '')
-    columnToDateTime(df, "Date", "%d %b %Y")
+    column_to_date_time(df, "Date", "%d %b %Y")
 
     df.rename(columns={"Unnamed: 2": "Space1"}, inplace=True) # Rename empty column
-    insertNewColumn(df, 3, "Space2", [np.nan]*len(df))
-    insertNewColumn(df, 4, "Space3", [np.nan]*len(df))
+    insert_new_column(df, 3, "Space2", [np.nan]*len(df))
+    insert_new_column(df, 4, "Space3", [np.nan]*len(df))
     df["Amount"] = pd.to_numeric(df['Amount'])
 
     # df.to_excel('hi.xlsx')
     print(df)
     return df
 
-def setUpTD(td_sheet):
+def setup_credit_TD(td_sheet, is_debit=False):
     df = pd.read_csv(td_sheet, usecols=[0,1,2,3], names=["Date", "Description", "Amount", "Gained"])
-    df = removeCreditCardThankYou(df)
-    columnToDateTime(df, "Date", "%m/%d/%Y")
+    df = remove_credit_card_thank_you(df)
+
+    column_to_date_time(df, "Date", "%m/%d/%Y")
+
+    # Add empty columns in front of description column
     for i in range(2,5):
-        insertNewColumn(df, i, "Space" + str(i-1), [np.nan]*len(df))
+        insert_new_column(df, i, "Space" + str(i-1), [np.nan]*len(df))
 
     # Move gained value into amount as a negative and delete column
     df['Amount'] = df['Amount'].fillna(-df['Gained'])
     df = df.drop(df.columns[-1], axis=1)
     
+    # Only included e-transfer information for debit card
+    if is_debit:
+        filter = df["Description"].str.contains("SEND E-TFR|E-TRANSFER")
+        df = df[filter]
+
     # df.to_excel('hi2.xlsx')
     print(df)
     return df
@@ -70,11 +77,17 @@ if __name__ == "__main__":
     parser.add_argument('debit_td_sheet')
     args = parser.parse_args()
 
-    ae_df = setUpCreditAE(args.credit_ae_sheet)
-    print()
-    td_credit_df = setUpTD(args.credit_td_sheet)
-    print()
-    combine_df(ae_df, td_credit_df)
+    # ae_df = setup_credit_AE(args.credit_ae_sheet)
+    # print()
+    # td_credit_df = setup_credit_TD(args.credit_td_sheet)
+    # print()
+    # combine_df(ae_df, td_credit_df)
 
     # Consider doing e transfer only for td
-    # td_debit_df = setUpTD(args.debit_td_sheet)
+    td_debit_df = setup_credit_TD(args.debit_td_sheet, is_debit=True)
+
+# Consider creating a masters excel file that concats everything to it
+# In that masters excel, when you concat everything make sure you dont concat duplicates
+# Be able to divide that up the masters into months
+# Implement the debit sheet by only including etransfers
+# Get the actual date to show and not just datetime
