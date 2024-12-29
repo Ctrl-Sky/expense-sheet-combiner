49c77703483ae6aae6152c541ed5fdba27494ab8
Ctrl-Sky <scquan23@gmail.com>
2024-12-28 21:33:16 -0500

    Concat with existing excel

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-from openpyxl import load_workbook
-import pandas as pd
 from standardize import initialize_AE, initialize_TD
 from combine_and_split import combine_and_split_by_month
 from write import write_to_master
@@ -10,26 +8,3 @@ if __name__ == "__main__":
     td_debit_df = initialize_TD("sheets/accountactivity.csv", is_debit=True)
     split_df = combine_and_split_by_month(ae_df, td_credit_df, td_debit_df)
     write_to_master(split_df)
-
-
-    # book = load_workbook('sheets/master.xlsx')
-    # writer = pd.ExcelWriter('sheets/master.xlsx', engine='openpyxl')
-    # # writer.book = book
-
-    # for month, sub_df in split_df.items():
-    #     print(f"Data for {month}:\n{sub_df}\n")
-    #     sub_df.to_excel(writer, sheet_name=month, startrow=writer.sheets[month].max_row, index=False, header=False)
-
-
-    # writer.save()
-
-    # Things to do:
-    # Need to resize columns again
-    # Need to restructure code again
-
-# Theory craft for removing duplicates
-# Pull existing info from sheets into a df
-# combine that df with the new one
-# df = df.drop_duplicates(subset=['Description', 'Amount'], keep='first')
-# Creates table that drops duplicate
-# Write that to the sheet
--- src/write.py
+++ src/write.py
@@ -9,18 +9,24 @@ def alter_column_width(sheetname, column, width):
     worksheet.column_dimensions[column].width = width
     workbook.save(MASTER_PATH)
 
+def concat_with_existing_df(df, sheetname):
+    existing_df = pd.read_excel(MASTER_PATH, sheet_name=sheetname)
+    new_df = pd.concat([existing_df, df])
+    new_df.drop_duplicates(['Description', 'Amount'], inplace=True, keep='last')
+    return new_df
+
 def write_to_master(split_df):
     for month, sub_df in split_df.items():
         sub_df['Date'] = sub_df['Date'].dt.date
         try:
             with pd.ExcelWriter(MASTER_PATH, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
                 if month in writer.sheets:
-                    startrow = writer.sheets[month].max_row
-                    sub_df.to_excel(writer, sheet_name=month, startrow=startrow, index=False, header=False)
+                    new_df = concat_with_existing_df(sub_df, sheetname=month)
+                    new_df.to_excel(writer, sheet_name=month, index=False)
                 else:
                     sub_df.to_excel(writer, sheet_name=month, index=False)
         except FileNotFoundError:
             with pd.ExcelWriter(MASTER_PATH, engine='openpyxl') as writer:
                 sub_df.to_excel(writer, sheet_name=month, index=False)
 
-        alter_column_width(month, 'A', 11)
+        alter_column_width(month, 'A', 11) # Increase width of date column tp see full value
