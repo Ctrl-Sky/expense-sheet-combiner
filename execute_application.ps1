2171fbf86733e9a8e59760c37c3cf13614cd8cdf
Ctrl-Sky <scquan23@gmail.com>
2025-06-07 00:52:57 -0400

    add git push

--- execute_application.ps1
+++ execute_application.ps1
@@ -1,19 +1,30 @@
-# Ensure repo is up-to-date
+# Powershell equivalent of execute_application.sh
+
+# Keep up to date
 git pull
 
-# Setup Virtual Environment
-python3.12 -m venv .venv
+# Install Dependencies
+python -m venv .venv
 .\.venv\Scripts\activate
+
+# Install required packages
 pip install -r requirements.txt
 
-# Run validation
-Write-Host "Validating..."
-python ./verify_sheets.py
-Write-Host "Completed"
+# Get variables from job_description.txt
+$COMPANY_NAME = python utils/get_company_name.py
+$JOB_TITLE = python utils/get_job_title.py
 
-# Run python script
-python ./src/main.py true
+# Generate Resume and Cover Letter
+python src/main.py
+
+# Store and save resume and cover letter
+.\cleanup_scripts\store_resume_and_cl.ps1 $COMPANY_NAME $JOB_TITLE
+python cleanup_scripts\save_to_csv.py $COMPANY_NAME $JOB_TITLE
+
 Write-Host "Application successfully executed"
 
-# Exit venv
+# Break down
 deactivate
+git add .
+git commit -m "(Automated Commit) Job application updated"
+git push
--- execute_application.sh
+++ execute_application.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-# Ensure repo is up-to-date
+# Keep up to date
 git pull
 
-# Setup Virtual Environment
+# Install Dependencies
 python3.12 -m venv .venv
 . .venv/bin/activate
 pip install -r requirements.txt
 
-# Run validation
-echo "Validating..."
-python verify_sheets.py
-echo "Completed"
+# Get variables from job_description.txt
+export COMPANY_NAME=$(python3.12 utils/get_company_name.py)
+export JOB_TITLE=$(python3.12 utils/get_job_title.py)
 
-# Run python script
-python src/main.py true
+# Generate Resume and Cover Letter
+python3.12 src/main.py
+
+# Store and save resume and cover letter
+bash cleanup_scripts/store_resume_and_cl.sh "${COMPANY_NAME}" "${JOB_TITLE}"
+python3.12 cleanup_scripts/save_to_csv.py "${COMPANY_NAME}" "${JOB_TITLE}"
+
 echo "Application successfully executed"
 
-# Exit venv
+# Break down
 deactivate
+git add .
+git commit -m "(Automated Commit) Job application updated"
+git push
